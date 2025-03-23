3660d970f2fb1a6a38a8934c84e3997b8ee67ddd
vscalar <vscalar02@gmail.com>
2025-03-23 21:09:40 +0900

    solved 9012.py

---
+++ 25-1 ALPS/week2/9012.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+#https://www.acmicpc.net/problem/32864
+
+if __name__ == "__main__":
+    
+    #get N
+    N = int(input())
+
+    for _ in range(N):
+        
+        #make a stack
+        stack = []
+
+        #get input
+        ps = input().strip()
+        #check vps
+        is_vps = True 
+
+        for p in ps:
+            #not vps
+            if not is_vps:          
+                break
+
+            if p == "(":
+                stack.append("(")
+            #rp occured when there's no lp
+            elif len(stack) == 0:
+                is_vps = False
+                continue
+
+            else:
+                stack.pop()
+        #used every string
+        if is_vps and len(stack) == 0:
+            print("YES")
+        else:
+            print("NO")
