22c1b9ceba2b023bd8beddc63084b7c60736380b
vscalar <vscalar02@gmail.com>
2025-03-26 18:31:20 +0900

    solved 15649.py

---
+++ 25-1 ALPS/week3/15649.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+#https://www.acmicpc.net/problem/15649
+
+#make a seq and print
+def dfs(stack:list, N:int, M:int) -> None:
+    if len(stack) == M:
+        #print the stack
+        print(' '.join(map(str, stack)))
+        return
+    
+    for i in range(1, N+1):
+        if i not in stack:
+            #add a num not in stack
+            stack.append(i)
+            #pick rest of the nums
+            dfs(stack, N, M)
+            #find other cases
+            stack.pop()
+
+if __name__ == "__main__":
+    #get N, M
+    N, M = map(int, input().split())
+    stack = []
+    dfs(stack,N,M)
