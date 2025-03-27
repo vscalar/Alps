aa30b5178bf6b76bcc193fd3033f906a60099d38
vscalar <vscalar02@gmail.com>
2025-03-27 15:52:58 +0900

    solved 15655.py

---
+++ 25-1 ALPS/week3/15655.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+#https://www.acmicpc.net/problem/15654
+
+#make a seq and print
+def dfs(stack:list[int], nums:list[int], N:int, M:int) -> None:
+    nums.sort()
+    if len(stack) == M:
+        #print the stack
+        print(' '.join(map(str, stack)))
+        return
+    
+    for i in range(N):
+        #ignore if stack top is larger
+        if stack and stack[-1] >= nums[i]:
+            continue
+        stack.append(nums[i])
+        dfs(stack, nums, N, M)
+        #find other cases
+        stack.pop()
+
+if __name__ == "__main__":
+    #get N, M
+    N, M = map(int, input().split())
+    #get sorted nums
+    nums = list(map(int, input().split()))
+    nums.sort()
+    stack = []
+    dfs(stack,nums, N,M)
